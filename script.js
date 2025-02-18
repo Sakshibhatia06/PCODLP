b24814ead9c44d6f7f13e786a9d2cf2a34c1cb1d
Sakshi bhatia <bhatiasakshi60@gmail.com>
2025-02-18 11:36:49 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,30 @@
+document.addEventListener("DOMContentLoaded", function () {
+    let steps = document.querySelectorAll(".step");
+    steps.forEach((step, index) => {
+      setTimeout(() => {
+        step.style.opacity = "1";
+      }, index * 500);
+    });
+  });
+document.addEventListener("DOMContentLoaded", function () {
+    let cards = document.querySelectorAll(".card");
+    cards.forEach((card, index) => {
+      setTimeout(() => {
+        card.style.opacity = "1";
+      }, index * 400);
+    });
+  });
+  document.addEventListener("DOMContentLoaded", function () {
+    let points = document.querySelectorAll(".fail-point");
+    points.forEach((point, index) => {
+      setTimeout(() => {
+        point.style.opacity = "1";
+      }, index * 400);
+    });
+  });
+  document.querySelectorAll('.faq-question').forEach(item => {
+    item.addEventListener('click', function() {
+      const answer = item.nextElementSibling;
+      answer.style.display = answer.style.display === "block" ? "none" : "block";
+    });
+  });
