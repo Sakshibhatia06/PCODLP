ab3c39eeff4e9e630b8ae26d324a7e1b0239e7f8
SAKSHI BHATIA <145453542+Sakshibhatia06@users.noreply.github.com>
2025-02-21 13:23:50 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,44 @@
+document.addEventListener("DOMContentLoaded", function () {
+    let steps = document.querySelectorAll(".step");
+    steps.forEach((step, index) => {
+      setTimeout(() => {
+        step.style.opacity = "1";
+      }, index * 500);
+    });
+  });
+document.addEventListener("DOMContentLoaded", function () {
+    let cards = document.querySelectorAll(".card");
+    cards.forEach((card, index) => {
+      setTimeout(() => {
+        card.style.opacity = "1";
+      }, index * 400);
+    });
+  });
+  document.addEventListener("DOMContentLoaded", function () {
+    let points = document.querySelectorAll(".fail-point");
+    points.forEach((point, index) => {
+      setTimeout(() => {
+        point.style.opacity = "1";
+      }, index * 400);
+    });
+  });
+  document.querySelectorAll('.faq-question').forEach(item => {
+    item.addEventListener('click', function() {
+      const answer = item.nextElementSibling;
+      answer.style.display = answer.style.display === "block" ? "none" : "block";
+    });
+  });
+  function openPopup() {
+    document.getElementById("popup").style.display = "flex";
+}
+
+// Close Popup
+function closePopup() {
+    document.getElementById("popup").style.display = "none";
+}
+
+// Submit Form (For now, just close the popup)
+function submitForm() {
+    alert("Form Submitted Successfully!");
+    closePopup();
+}
